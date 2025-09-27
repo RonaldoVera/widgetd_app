eb7048d5ec3b672bd4f95bf5d85590b4a7715007
ronaldoveraparra <ronaldo.vera@efby.cl>
2025-09-27 14:00:11 -0300

    debugShowCheckedModeBanner: false

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello Rone!!!!!'))),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello Rone!!!'))),
     );
   }
 }
