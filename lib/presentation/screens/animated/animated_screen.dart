3d8683337867e889e767d334abfbef6f7ef88e5d
ronaldoveraparra <ronaldo.vera@efby.cl>
2025-09-29 22:54:23 -0300

    screens creadar y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class animatedScreen extends StatelessWidget {
+  static const String routeName = 'animated_screen';
+
+  const animatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
